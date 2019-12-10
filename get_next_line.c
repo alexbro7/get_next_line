6dd8cb8a45833de118a0f8f6786cdbd0226c9eb8
Alexandre BROSSETTE <alebross@e2r7p7.42.fr>
2019-12-10 14:46:32 +0100

    +*

---
+++ get_next_line.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alebross <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/09 18:20:11 by alebross          #+#    #+#             */
+/*   Updated: 2019/12/09 20:47:59 by alebross         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char *str;
+
+	str = (char*)s;
+	while (*str)
+	{
+		if ((char)c == *str)
+			return (str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return (str);
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*dest;
+	unsigned int	i;
+
+	dest = ft_strnew(len);
+	if (!s || !dest)
+		return (NULL);
+	i = 0;
+	while (i < (unsigned int)len && s[start + i])
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	dest = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!dest)
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (s1[++i])
+		dest[i] = s1[i];
+	while (s2[++j])
+		dest[i + j] = s2[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char *stock;
+	int ret;
+	char *buff;
+	int i;
+
+	if (fd <= 0 || !line || !(buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	return (-1);
+	if (!stock)
+		stock = malloc(sizeof(char));
+	while (!ft_strchr(buff, '\n') && (ret = read(fd, buff, BUFFER_SIZE)))
+	{
+		buff[BUFFER_SIZE] ='\0';
+		stock = ft_strjoin(stock, buff); 
+	}
+ 	i = 0;
+	while (stock[i] && stock[i] != '\n')
+		i++;
+	*line = ft_substr(stock, 0, i);
+	stock = ft_strdup(stock + i + 1);
+	return (ret > 0 ? 1 : 0);
+}
